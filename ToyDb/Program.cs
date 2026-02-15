8bfae1ce2201d03c83ac23f7c6029d24ec459e5d
Ryan Achten <ryanachten@gmail.com>
2026-02-15 16:34:14 +1300

    feat: add replica healthchecks

--- ToyDb/Program.cs
+++ ToyDb/Program.cs
@@ -4,11 +4,13 @@ using ToyDb.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddGrpcHealthChecks();
 
 builder.RegisterServiceCollection();
 
 var app = builder.Build();
 
 app.MapGrpcService<ClientService>();
+app.MapGrpcHealthChecksService();
 
 await app.RunAsync();
